4e9825c6485afa00d4161d7df424b9ea20362694
Luke Shillabeer <lshillabeer@gmail.com>
2014-02-11 15:18:01 +1100

    Made distributable version work

--- setup.py
+++ setup.py
@@ -8,10 +8,18 @@ setup(
     author='Luke Shillabeer',
     author_email='lshillabeer@gmail.com',
     packages=['primerlint'],
-    scripts=['primerlint/primerlint.py','primerlint/importfiles.py','primerlint/outputfiles.py','primerlint/analysismodules.py'],
-    entry_points={'console_scripts': ['primer-lint = primerlint:main']},
+    scripts=[
+        'primerlint/primerlint.py',
+        'primerlint/importfiles.py',
+        'primerlint/outputfiles.py',
+        'primerlint/analysismodules.py'
+        ],
+    entry_points={
+        'console_scripts': ['primer-lint = primerlint:main']
+        },
     description=(
         'Primer-Lint: A tool for analysing primers for the Hi-Plex \
-         targeted, multiplexed DNA sequencing strategy.'),
+         targeted, multiplexed DNA sequencing strategy.'
+         ),
     install_requires=['biopython >= 1.62'],
 )
